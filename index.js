20b249248c3217a1808c5e51a8cc66caf0f66ab9
Lewis <lewismunyambu@gmail.com>
2022-08-27 09:51:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 function hasTargetSum(array, target) {
-  const seenNumbers = new Set(); // initialize an empty Set
+  const seenNumbers = new Set(); 
   for (const number of array) {
     const complement = target - number;
-
-    // .has returns true if the Set includes the complement
     if (seenNumbers.has(complement)) return true;
-
-    // .add adds the number to the Set
     seenNumbers.add(number);
   }
   return false;
